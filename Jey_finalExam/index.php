c322d498c32a0cb92a6f48da3c7a95d3987d769e
jlegarie-cc <jlegarie-cc@CCWAT2D04STAC05.conestogac.on.ca>
2012-08-31 10:01:18 -0400

    Hello world!

--- Jey_finalExam/index.php
+++ Jey_finalExam/index.php
@@ -1,4 +1,25 @@
 <?php
+
+//setup database
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/examfinal',
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
+
+class examfinal extends ActiveRecord\Model{
+
+}
+
+
 ?>
 
 <html>
