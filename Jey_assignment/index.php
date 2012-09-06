0554011b54d4b6f48afcd216150a124c86b5d598
jlegarie-cc <jlegarie-cc@W2D04-C05.conestogac.on.ca>
2012-09-06 14:27:47 -0400

    Sorting by columns

--- Jey_assignment/index.php
+++ Jey_assignment/index.php
@@ -4,19 +4,6 @@ $footer='PlastiCrypt '.$today['year'].'&copy;, caretaker greyhaze.';
 $action = array_key_exists('action', $_POST)?$_POST['action']: '';
 $action = array_key_exists('action', $_GET)?$_GET['action']: $action;
 
-if ($action == "search"){
-	foreach($_POST['size']as $sz){
-		$s[]=$sz;
-	}
-	foreach($_POST['rarity']as $sz){
-		$r[]=$sz;
-	}
-	foreach($_POST['type']as $sz){
-		$t[]=$sz;
-	}
-}
-
-
 //setup database
 require '../ActiveRecord/ActiveRecord.php';
 
@@ -31,6 +18,126 @@ ActiveRecord\Config::initialize(function($cfg)
 			)
 	);
 });
+
+$oGallery = gallery::find('all', array('select' => 'size, rarity, type, origin, gender, class, weapons, armour, setname, productionline, manufacturer, streetdate'));
+	foreach($oGallery as $ogall){
+		$s[] = $ogall->size;
+		$r[] = $ogall->rarity;
+		$t[] = $ogall->type;
+		$o[] = $ogall->origin;
+		$g[] = $ogall->gender;
+		$c[] = $ogall->class;
+		$w[] = $ogall->weapons;
+		$a[] = $ogall->armour;
+//		$m[] = $ogall->source;
+//		$st[] = $ogall->stats;
+		$set[] = $ogall->setname;
+		$p[] = $ogall->productionline;
+		$f[] = $ogall->manufacturer;
+		$e[] = $ogall->streetdate;
+	}	
+$e[] = $f[] = $p[] = $set[] = $st[] = $m[] = $a[] = $w[] = $c[] = $g[] = $o[] = $t[] = $r[] = $s[] = 'NA';
+$s = array_unique($s);
+$r = array_unique($r);
+$t = array_unique($t);
+$o = array_unique($o);
+$g = array_unique($g);
+$c = array_unique($c);
+$w = array_unique($w);
+$a = array_unique($a);
+//$m = array_unique($m);
+//$st = array_unique($st);
+$set = array_unique($set);
+$p = array_unique($p);
+$f = array_unique($f);
+$e = array_unique($e);
+
+if ($action == "search"){
+	if(isset($_POST['size'])){
+		$s='';
+		foreach($_POST['size']as $sz){
+			$s[]=$sz;
+		}
+	}
+	if(isset($_POST['rarity'])){
+		$r='';
+		foreach($_POST['rarity']as $sz){
+		$r[]=$sz;
+		}
+	}
+	if(isset($_POST['type'])){
+		$t='';
+		foreach($_POST['type']as $sz){
+		$t[]=$sz;
+		}
+	}
+	if(isset($_POST['origin'])){
+		$o='';
+		foreach($_POST['origin']as $sz){
+		$o[]=$sz;
+		}
+	}
+	if(isset($_POST['gender'])){
+		$g='';
+		foreach($_POST['gender']as $sz){
+		$g[]=$sz;
+		}
+	}
+	if(isset($_POST['class'])){
+		$c='';
+		foreach($_POST['class']as $sz){
+		$c[]=$sz;
+		}
+	}
+	if(isset($_POST['weapons'])){
+		$w='';
+		foreach($_POST['weapons']as $sz){
+		$w[]=$sz;
+		}
+	}
+	if(isset($_POST['armour'])){
+		$a='';
+		foreach($_POST['armour']as $sz){
+		$a[]=$sz;
+		}
+	}
+	if(isset($_POST['source'])){
+		$m='';
+		foreach($_POST['source']as $sz){
+		$m[]=$sz;
+		}
+	}
+	if(isset($_POST['stats'])){
+		$st='';
+		foreach($_POST['stats']as $sz){
+		$st[]=$sz;
+		}
+	}
+	if(isset($_POST['setname'])){
+		$set='';
+		foreach($_POST['setname']as $sz){
+		$set[]=$sz;
+		}
+	}
+	if(isset($_POST['productionline'])){
+		$p='';
+		foreach($_POST['productionline']as $sz){
+		$p[]=$sz;
+		}
+	}
+	if(isset($_POST['manufacturer'])){
+		$f='';
+		foreach($_POST['manufacturer']as $sz){
+		$f[]=$sz;
+		}
+	}
+	if(isset($_POST['streetdate'])){
+		$e='';
+		foreach($_POST['streetdate']as $sz){
+		$e[]=$sz;
+		}
+	}
+}
 
 ?>
 
@@ -113,12 +220,15 @@ ActiveRecord\Config::initialize(function($cfg)
 			<div class="gallery">
 				<?php 
 					if($action=='search'){
-
-//						foreach(gallery::find('all', array('order' => 'setnum', 'conditions' => array('type = ?', $_POST['type']))) as $oGallery){
-						foreach(gallery::find('all', array('conditions' => array('type in (?) AND origin in (?)', array('animal', 'construct', 'humanoid'), array('abyss', 'hell', 'material plane')))) as $oGallery){
-						
-							include('views/display_partial.php');
+						$flag = true;
+						foreach(gallery::find('all', array('conditions' => array('size in (?) AND rarity in (?) AND type in (?) AND origin in (?) AND gender in (?) AND class in (?) AND weapons in (?) AND armour in (?) AND setname in (?) AND productionline in (?) AND manufacturer in (?)', $s, $r, $t, $o, $g, $c, $w, $a, $set, $p, $f))) as $oGallery){
+						$flag = false;
+						include('views/display_partial.php');
 						}//foreach
+						if($flag){ ?>
+							<p class="message">Nothing fits your criteria.</p>
+							<?php
+						}
 					}else{
 
 						include('views/display.php');
--- Jey_assignment/views/search.php
+++ Jey_assignment/views/search.php
@@ -26,79 +26,81 @@ foreach(gallery::find('all', array('conditions' => array('type in (?) AND origin
 	<div class="item" style="display:block"></div>
 	<div class="item" style="display:none">
 	<fieldset><legend>SIZE: </legend>
-		<input type="checkbox" name="size[]" value="tiny" /> Tiny<br />
-		<input type="checkbox" name="size[]" value="small" /> Small<br />
-		<input type="checkbox" name="size[]" value="medium" /> Medium<br />
-		<input type="checkbox" name="size[]" value="large" /> Large 2x2<br />
-		<input type="checkbox" name="size[]" value="huge" /> Huge 3x3<br />
-		<input type="checkbox" name="size[]" value="gargantuan" /> Gargantuan 4x4<br />
-		<input type="checkbox" name="size[]" value="colossal" /> Colossal 6x6<br />
+		<input type="checkbox" name="size[]" value="Tiny" /> Tiny<br />
+		<input type="checkbox" name="size[]" value="Small" /> Small<br />
+		<input type="checkbox" name="size[]" value="Medium" /> Medium<br />
+		<input type="checkbox" name="size[]" value="Large" /> Large 2x2<br />
+		<input type="checkbox" name="size[]" value="Huge" /> Huge 3x3<br />
+		<input type="checkbox" name="size[]" value="Gargantuan" /> Gargantuan 4x4<br />
+		<input type="checkbox" name="size[]" value="Colossal" /> Colossal 6x6<br />
 	</fieldset><fieldset><legend>RARITY: </legend>
-		<input type="checkbox" name="rarity" value="none" /> No Rarity<br />
-		<input type="checkbox" name="rarity" value="common" /> Common<br />
-		<input type="checkbox" name="rarity" value="uncommon" /> Uncommon<br />
-		<input type="checkbox" name="rarity" value="rare" /> Rare<br />
+		<input type="checkbox" name="rarity[]" value="None" /> No Rarity<br />
+		<input type="checkbox" name="rarity[]" value="Common" /> Common<br />
+		<input type="checkbox" name="rarity[]" value="Uncommon" /> Uncommon<br />
+		<input type="checkbox" name="rarity[]" value="Rare" /> Rare<br />
 	</fieldset><fieldset><legend>TYPE: </legend>
-		<input type="checkbox" name="type" value="aberration" /> Aberration<br />
-		<input type="checkbox" name="type" value="animal" /> Animal<br />
-		<input type="checkbox" name="type" value="construct" /> Construct<br />
-		<input type="checkbox" name="type" value="giant" /> Giant<br />
-		<input type="checkbox" name="type" value="humanoid" /> Humanoid<br />
-		<input type="checkbox" name="type" value="magical beast" /> Magical Beast<br />
-		<input type="checkbox" name="type" value="monstrous humanoid" /> Monstrous Humanoid<br />
-		<input type="checkbox" name="type" value="outsider" /> Outsider<br />
-		<input type="checkbox" name="type" value="plant" /> Plant<br />
-		<input type="checkbox" name="type" value="undead" /> Undead<br />
+		<input type="checkbox" name="type[]" value="Aberration" /> Aberration<br />
+		<input type="checkbox" name="type[]" value="Animal" /> Animal<br />
+		<input type="checkbox" name="type[]" value="Construct" /> Construct<br />
+		<input type="checkbox" name="type[]" value="Giant" /> Giant<br />
+		<input type="checkbox" name="type[]" value="Humanoid" /> Humanoid<br />
+		<input type="checkbox" name="type[]" value="Magical Beast" /> Magical Beast<br />
+		<input type="checkbox" name="type[]" value="Monstrous Humanoid" /> Monstrous Humanoid<br />
+		<input type="checkbox" name="type[]" value="Outsider" /> Outsider<br />
+		<input type="checkbox" name="type[]" value="Plant" /> Plant<br />
+		<input type="checkbox" name="type[]" value="Undead" /> Undead<br />
 	</fieldset><fieldset><legend>PLACE OF ORIGIN: </legend>
-		<input type="checkbox" name="origin" value="abyss" /> Abyss<br />
-		<input type="checkbox" name="origin" value="hell" /> Hell<br />
-		<input type="checkbox" name="origin" value="material plane" /> Material Plane<br />
+		<input type="checkbox" name="origin[]" value="Abyss" /> Abyss<br />
+		<input type="checkbox" name="origin[]" value="Hell" /> Hell<br />
+		<input type="checkbox" name="origin[]" value="Material Plane" /> Material Plane<br />
 	</fieldset><fieldset><legend>GENDER: </legend>
-		<input type="checkbox" name="gender" value="na" /> NA<br />
-		<input type="checkbox" name="gender" value="female" /> Female<br />
-		<input type="checkbox" name="gender" value="male" /> Male<br />
+		<input type="checkbox" name="gender[]" value="NA" /> NA<br />
+		<input type="checkbox" name="gender[]" value="Female" /> Female<br />
+		<input type="checkbox" name="gender[]" value="Male" /> Male<br />
 	</fieldset><fieldset><legend>CLASS: </legend>
-		<input type="checkbox" name="class" value="na" /> NA<br />
-		<input type="checkbox" name="class" value="cleric" /> Cleric<br />
-		<input type="checkbox" name="class" value="fighter" /> Fighter<br />
-		<input type="checkbox" name="class" value="wizard" /> Wizard<br />
+		<input type="checkbox" name="class[]" value="NA" /> NA<br />
+		<input type="checkbox" name="class[]" value="Cleric" /> Cleric<br />
+		<input type="checkbox" name="class[]" value="Fighter" /> Fighter<br />
+		<input type="checkbox" name="class[]" value="Noble" /> Noble<br />
+		<input type="checkbox" name="class[]" value="Warrior" /> Warrior<br />
+		<input type="checkbox" name="class[]" value="Wizard" /> Wizard<br />
 	</fieldset><fieldset><legend>WEAPONS: </legend>
-		<input type="checkbox" name="weapon" value="unarmed" /> Unarmed<br />
-		<input type="checkbox" name="weapon" value="bowcrossbow" /> Bow/Crossbow<br />
-		<input type="checkbox" name="weapon" value="dualwielding" /> Dual Wielding<br />
-		<input type="checkbox" name="weapon" value="exotic" /> Exotic Weapon<br />
-		<input type="checkbox" name="weapon" value="staffwand" /> Staff/Wand/Scepter<br />
-		<input type="checkbox" name="weapon" value="swordboard" /> Sword/Board<br />
-		<input type="checkbox" name="weapon" value="twohanded" /> Two Handed Weapon<br />
+		<input type="checkbox" name="weapons[]" value="Unarmed" /> Unarmed<br />
+		<input type="checkbox" name="weapons[]" value="Cane" /> Cane<br />
+		<input type="checkbox" name="weapons[]" value="Dual Wielding" /> Dual Wielding<br />
+		<input type="checkbox" name="weapons[]" value="Exotic" /> Exotic Weapon<br />
+		<input type="checkbox" name="weapons[]" value="Staff" /> Staff<br />
+		<input type="checkbox" name="weapons[]" value="Polearm" /> Polearm<br />
+		<input type="checkbox" name="weapons[]" value="Pick" /> Pick<br />
 	</fieldset><fieldset><legend>ARMOUR: </legend>
-		<input type="checkbox" name="armour" value="Unarmoured" /> Unarmoured<br />
-		<input type="checkbox" name="armour" value="LightArmour" /> Light Armour<br />
-		<input type="checkbox" name="armour" value="MediumArmour" /> Medium Armour<br />
-		<input type="checkbox" name="armour" value="HeavyArmour" /> Heavy Armour<br />
+		<input type="checkbox" name="armour[]" value="Unarmoured" /> Unarmoured<br />
+		<input type="checkbox" name="armour[]" value="Light" /> Light Armour<br />
+		<input type="checkbox" name="armour[]" value="Medium" /> Medium Armour<br />
+		<input type="checkbox" name="armour[]" value="Heavy" /> Heavy Armour<br />
 	</fieldset><fieldset><legend>MANUALS: </legend>
-		<input type="checkbox" name="entry" value="FiendFolio" /> Fiend Folio<br />
-		<input type="checkbox" name="entry" value="MonsterManual" /> Monster Manual<br />
-		<input type="checkbox" name="entry" value="MonsterManual2" /> Monster Manual 2<br />
+		<input type="checkbox" name="source[]" value="FiendFolio" /> Fiend Folio<br />
+		<input type="checkbox" name="source[]" value="MonsterManual" /> Monster Manual<br />
+		<input type="checkbox" name="source[]" value="MonsterManual2" /> Monster Manual 2<br />
 	</fieldset><fieldset><legend>STATS: </legend>
-		<input type="checkbox" name="stats" value="DDM" /> DDM<br />
-		<input type="checkbox" name="stats" value="DDM2" /> DDM2.0<br />
-		<input type="checkbox" name="stats" value="Heroscape" /> Heroscape<br />
+		<input type="checkbox" name="stats[]" value="DDM" /> DDM<br />
+		<input type="checkbox" name="stats[]" value="DDM2.0" /> DDM2.0<br />
+		<input type="checkbox" name="stats[]" value="Heroscape" /> Heroscape<br />
 	</fieldset><fieldset><legend>SET: </legend>
-		<input type="checkbox" name="set" value="PFB_HaM" /> Heroes &amp; Monsters<br />
-		<input type="checkbox" name="set" value="PFB_RotR" /> Rise of the Runelords<br />
-		<input type="checkbox" name="set" value="PFB_SS" /> Shattered Star<br />
+		<input type="checkbox" name="setname[]" value="PFB_HaM" /> Heroes &amp; Monsters<br />
+		<input type="checkbox" name="setname[]" value="PFB_RotR" /> Rise of the Runelords<br />
+		<input type="checkbox" name="setname[]" value="PFB_SS" /> Shattered Star<br />
 	</fieldset><fieldset><legend>PRODUCTION LINE: </legend>
-		<input type="checkbox" name="productionline" value="DCM" /> Dungeon Crawler Minis (DCM)<br />
-		<input type="checkbox" name="productionline" value="DDM" /> Dungeon &amp; Dragons Miniatures (DDM)<br />
-		<input type="checkbox" name="productionline" value="PFB" /> Pathfinder Battles (PFB)<br />
+		<input type="checkbox" name="productionline[]" value="DCM" /> Dungeon Crawler Minis (DCM)<br />
+		<input type="checkbox" name="productionline[]" value="DDM" /> Dungeon &amp; Dragons Miniatures (DDM)<br />
+		<input type="checkbox" name="productionline[]" value="PFB" /> Pathfinder Battles (PFB)<br />
 	</fieldset><fieldset><legend>MANUFACTURER: </legend>
-		<input type="checkbox" name="manufacturer" value="GVN" /> Gifted Vision inc. (GVN)<br />
-		<input type="checkbox" name="manufacturer" value="PZO" /> Paizo / WizKids (PZO)<br />
-		<input type="checkbox" name="manufacturer" value="WotC" /> Wizards of the Coast (WotC)<br />
+		<input type="checkbox" name="manufacturer[]" value="GVN" /> Gifted Vision inc. (GVN)<br />
+		<input type="checkbox" name="manufacturer[]" value="PZO" /> Paizo / WizKids (PZO)<br />
+		<input type="checkbox" name="manufacturer[]" value="WotC" /> Wizards of the Coast (WotC)<br />
 	</fieldset><fieldset><legend>STREET DATE BY YEAR: </legend>
-		<input type="checkbox" name="street" value="2012" /> 2012<br />
-		<input type="checkbox" name="street" value="2011" /> 2011<br />
-		<input type="checkbox" name="street" value="2010" /> 2010<br />
+		<input type="checkbox" name="streetdate[]" value="2012" /> 2012<br />
+		<input type="checkbox" name="streetdate[]" value="2011" /> 2011<br />
+		<input type="checkbox" name="streetdate[]" value="2010" /> 2010<br />
 	</fieldset>
 </div>
 </form>
